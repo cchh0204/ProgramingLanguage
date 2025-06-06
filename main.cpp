6307eb3ddf159a41117574119e2d03f15831884b
ChungHo Lee <cchh0204@yu.ac.kr>
2025-06-07 02:36:15 +0900

    첫번째 커밋: 1-1과제 완료 코드

--- main.cpp
+++ main.cpp
@@ -3,9 +3,37 @@
 #include<map>
 #include<vector>
 #include<fstream>
-#define PATH "storage.txt"
-
+#define PATH "list.txt"
 using namespace std;
+
+class Account {
+	string code; // 계좌번호
+	string owner; //예금주
+	int bal; //잔액
+	string password; // 비밀번호
+public:
+	Account(string code =" ", string owner = " " , int bal = 0, string password = " ");
+	string getCode() { return code; };
+	string getOwner() { return owner; };
+	int getBal() { return bal; };
+	string getPassword() { return password; };
+
+	void setOwner(string newowner) { this->owner = newowner; }
+	void setBal(int newbal) { this->bal = newbal; }
+	void setPassword(string newpassword) { this->password = newpassword; }
+	
+};
+
+Account::Account(string code, string owner, int bal, string password) {
+	this->code = code;
+	this->owner = owner;
+	this->bal = bal;
+	this->password = password;
+}
+
+
+
+
 vector<string> split(string str, string delimiter)
 {
 	vector<string> v;
@@ -24,7 +52,7 @@ vector<string> split(string str, string delimiter)
 	return v;
 }
 
-bool fileread(string path, map<string, int>& storage)
+bool fileread(string path, map<string, Account>& accounts)
 {
 	ifstream fin(path);
 	if (!fin) {
@@ -38,14 +66,24 @@ bool fileread(string path, map<string, int>& storage)
 	while (getline(fin, buf))
 	{
 		vector<string> line = split(buf, " ");
-		//cout << buf << endl;
-		storage[line[0]] = stoi(line[1]);
+		
+		if (line.size() == 4) {
+			string code = line[0];
+			string owner = line[1];
+			int bal = stoi(line[2]); // 문자열 "10000"을 정수 10000으로 변환
+			string password = line[3];
+
+			accounts[code] = Account(code, owner, bal, password);
+		}
+		else {
+			cout << "올바르지 않습니다." << endl;
+		}
 	}
 	fin.close();
 	return 1;
 }
 
-bool filewrite(string path, map<string, int> storage)
+bool filewrite(string path, map<string, Account>& accounts)
 {
 	ofstream fout(path);
 	if (!fout) { // 열기 실패 검사
@@ -53,105 +91,108 @@ bool filewrite(string path, map<string, int> storage)
 		return 0;
 	}
 
-	for (auto item : storage)
+	for (auto item : accounts)
 	{
-		fout << item.first << " " << item.second << endl;
+		Account& acc = item.second;
+		fout << acc.getCode() << " "
+			<< acc.getOwner() << " "
+			<< acc.getBal() << " "
+			<< acc.getPassword() << endl;
 	}
 	fout.close();
+	return 1;
 }
 
 
-void showMap(map<string, int>& storage)
+void showAccounts(map<string, Account>& accounts)
 {
-	for (auto item : storage)
+	for (auto& pair : accounts)
 	{
-		cout << item.first << ":" << item.second << endl;
+		Account& acc = pair.second;
+		cout << acc.getCode() << " " << acc.getOwner() << endl;
 	}
 }
-
-
-#include<iostream>
-#include<string>
-#include<map>
-using namespace std;
 
 int main()
 {
-	map<string, int> s;
-	map<string, int>::iterator it; //자료구조 조회를 위한 포인터
-	string prod;
-	int cnt;
+	map<string, Account> accounts;
+	map<string, Account>::iterator it;
+	string code, owner, password;
+	int bal;
 	int select = 0;
 
-	fileread(PATH, s); // s가 map,즉 저장소
+	fileread(PATH, accounts); 
 
 
 	while (select != 4)
 	{
-		cout << "1.입고  2.출고  3.현황  4.종료 : ";
+		cout << "1.계좌등록  2.계좌정보수정  3.현황보기  4.종료 : ";
 		cin >> select;
 		switch (select)
 		{
-		case 1:
-			//입고리처리부
-			cout << "상품명 : "; cin >> prod;
-			cout << "수량 : "; cin >> cnt;
+		case 1: {
+			cout << "##계좌등록" << endl;
+			cout << "계좌번호 : "; cin >> code;
+			cout << "예금주 : "; cin >> owner;
+			cout << "잔액 : "; cin >> bal;
+			cout << "비밀번호 : "; cin >> password;
+			Account newAccount(code, owner, bal, password);
 
-			if (s.find(prod) == s.end()) //재고에 제품이 등록되지 않았다면.
+			if (accounts.find(code) == accounts.end()) // 계좌번호가 없다면.
 			{
-				s[prod] = cnt; // prod라는 제품이 cnt개 최초 등록됨
+				accounts[code] = newAccount;
+				
+				filewrite(PATH, accounts);
+
+				cout << "등록되었습니다. " << endl;
+				cout << endl;
 			}
 			else
 			{
-				s[prod] += cnt; // 기존 등록정보가 있으면 cnt만큼 증가
+				cout << "이미 등록된 계좌입니다." << endl;
+				cout << endl;
 			}
-			cout << "입고되었습니다" << endl;
 
-			filewrite(PATH, s);
-
+			
 			break;
+		}
 		case 2:
-			//출고처리부
-			cout << "상품명 : "; cin >> prod;
-			cout << "수량 : "; cin >> cnt;
-
-			if (s.find(prod) == s.end()) //재고에 제품이 등록되지 않았다면.
-			{
-				cout << "상품이 재고에 없습니다." << endl;
-			}
-			else
-			{
-				if (s[prod] < cnt) {
-					cout << "출고량이 재고량 보다 많습니다. 전부 출고합니다." << endl;
-					s[prod] = 0;
-					s.erase(prod);
-					filewrite(PATH, s);
-				}
-				else {
-					s[prod] -= cnt; // 기존 등록정보가 있으면 cnt만큼 감소
-					filewrite(PATH, s);
-					if (s[prod] == 0) {
-						s.erase(prod);
-						filewrite(PATH, s);
-					}
-					cout << "출고되었습니다" << endl;
-				}
-			}
-			
 			
 
+			cout << "##계좌수정" << endl;
+			cout << "계좌번호 : "; cin >> code;
+
+			it = accounts.find(code);
+
+			if (it != accounts.end()) {
+				cout << "예금주: "; cin >> owner;
+				cout << "잔액: "; cin >> bal;
+				cout << "비밀번호: "; cin >> password;
+				
+				it->second.setOwner(owner);
+				it->second.setBal(bal);
+				it->second.setPassword(password);
+
+				filewrite(PATH, accounts);
+
+				cout << "변경되었습니다. " << endl;
+			}
+			else 
+				cout << "존재하지 않는 계좌입니다." << endl;
+			
 			break;
+
 		case 3:
-			//현황처리
-			for (it = s.begin(); it != s.end(); it++)
-			{
-				cout << it->first << " : " << it->second << endl;
-			}
-
+			cout << "##현황보기" << endl;
+			showAccounts(accounts);
+			cout << endl;
 			break;
+
 		case 4:
 			cout << "종료합니다" << endl;
+			cout << endl;
 			break;
+
 		default:;
 		}
 	}
